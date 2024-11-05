c0d573a33d40acebdd5f929f3c3a8380eb30a020
Elinavet <freeefauna@gmail.com>
2024-11-05 16:39:44 +0000

    add vote buttons and update votes for single article

--- nc-news/src/components/SingleArticle.jsx
+++ nc-news/src/components/SingleArticle.jsx
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom"
-import { getArticleById } from "../utils/api";
+import { getArticleById, voteOnArticle } from "../utils/api";
 
 const SingleArticle = () =>{
   const { article_id } = useParams()
   const [article, setArticle] = useState({})
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(()=>{
-    getArticleById(article_id).then((articleData)=>{
-      setArticle(articleData)
-    })
-  }, [article_id])
+  useEffect(() => {
+    getArticleById(article_id).then((articleData) => {
+      setArticle(articleData);
+      setIsLoading(false);
+    }).catch((err) => {
+      setError(err);
+      setIsLoading(false);
+    });
+  }, [article_id]);
+
+
+
+  const handleVote = (vote) => {
+    voteOnArticle(article_id, vote)
+      .then((updatedArticle) => {
+        setArticle((previousArticle) => ({
+          ...previousArticle,
+          votes: updatedArticle.votes
+        }));
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+
+  if (isLoading) return <p>Loading article...</p>;
+  if (error) return <p>Error loading article: {error.message}</p>;
 
   return (
     <div id='single-article'>
@@ -21,6 +45,8 @@ const SingleArticle = () =>{
       <p>Created by {article.author} at {article.created_at}</p>
       <Link to={`/articles/${article_id}/comments`}>Comments: {article.comment_count}</Link>
       <p>Votes: {article.votes}</p>
+      <button onClick={() => handleVote(1)}>Upvote</button>
+      <button onClick={() => handleVote(-1)}>Downvote</button>
     </div>
   )
 }
